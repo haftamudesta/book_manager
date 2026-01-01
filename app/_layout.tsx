79a1bad2a655e44b379507e1d45e0286e2ea5eb1
Haftamu Desta <haftamudesta>
2025-12-31 20:13:12 -0800

    Add colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,21 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { Colors } from "@/constants/colors";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const theme = colorScheme ? Colors[colorScheme] : Colors.light;
+
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: theme.navBackground },
+        headerTintColor: theme.title,
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Home" }} />
       <Stack.Screen name="about" options={{ title: "About" }} />
-      <Stack.Screen name="contact" options={{ title: "contact" }} />
+      <Stack.Screen name="contact" options={{ title: "Contact" }} />
     </Stack>
   );
 }
--- app/about.tsx
+++ app/about.tsx
@@ -1,12 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { useColorScheme } from "react-native";
+import { Colors } from "@/constants/colors";
+import { Link } from "expo-router";
 
-const about = () => {
+const About = () => {
+  const colorScheme = useColorScheme();
+  const theme = colorScheme ? Colors[colorScheme] : Colors.light;
+
   return (
-    <View>
-      <Text>about</Text>
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <Text style={styles.title}>About</Text>
+      <Link href="/" style={styles.link}>
+        Back to Home
+      </Link>
     </View>
   );
 };
 
-export default about;
+export default About;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  link: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#007AFF",
+    color: "white",
+    borderRadius: 5,
+    textDecorationLine: "none",
+  },
+});
