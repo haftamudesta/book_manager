ee7f19b437da25b5a78969f027ac898b6038d795
Haftamu Desta <haftamudesta>
2026-01-02 23:34:30 -0800

    Add Sign up component

---
+++ app/(auth)/sign_up.tsx
@@ -0,0 +1,43 @@
+import Spacer from "@/components/Spacer";
+import ThemedText from "@/components/Themedtext";
+import ThemedView from "@/components/Themedview";
+import { Link } from "expo-router";
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const SignUp = () => {
+  return (
+    <ThemedView style={styles.container}>
+      <Spacer height={24} />
+      <ThemedText bold={true} variant="heading" style={styles.title}>
+        Sign Up To Get Started
+      </ThemedText>
+      <Spacer height={24} />
+
+      <ThemedText>
+        Have an account? Please{" "}
+        <Link href="/sign_up" style={styles.link}>
+          Sign In
+        </Link>
+      </ThemedText>
+    </ThemedView>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 24,
+    marginBottom: 30,
+  },
+  link: {
+    color: "blue",
+  },
+});
