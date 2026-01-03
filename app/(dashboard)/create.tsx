3c56784261e1f827442f92b2708e127e82bc01c3
Haftamu Desta <haftamudesta>
2026-01-03 02:29:40 -0800

    Add pages in dashboard

---
+++ app/(dashboard)/create.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+import Spacer from "../../components/Spacer";
+
+import ThemedText from "@/components/Themedtext";
+import ThemedView from "@/components/Themedview";
+
+const Create = () => {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText bold={true} style={styles.heading}>
+        Add a New Book
+      </ThemedText>
+      <Spacer />
+    </ThemedView>
+  );
+};
+
+export default Create;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
