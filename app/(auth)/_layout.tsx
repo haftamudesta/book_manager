90bb213036eaa08338a69fb24620955930240f74
Haftamu Desta <haftamudesta>
2026-01-09 01:17:23 -0800

    Add route protection

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { StatusBar, useColorScheme } from "react-native";
 import { Colors } from "@/constants/colors";
 import { useAuth } from "@/hooks/useUser";
+import GuestOnly from "@/components/auth/GuestOnly";
 
 export default function AuthLayout() {
   const colorScheme = useColorScheme();
@@ -9,7 +10,7 @@ export default function AuthLayout() {
   const { user } = useAuth();
   console.log("current iser", user);
   return (
-    <>
+    <GuestOnly>
       <StatusBar
         barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
         backgroundColor={theme.background}
@@ -20,6 +21,6 @@ export default function AuthLayout() {
           animation: "none",
         }}
       ></Stack>
-    </>
+    </GuestOnly>
   );
 }
